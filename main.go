cec42eec127c232b4fa6a13e8a7fb53c3cd5b4c3
Christopher Kotfila <chris.kotfila@kitware.com>
2020-04-26 20:43:02 -0400

    Use json instead of string format

--- main.go
+++ main.go
@@ -1,33 +1,59 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"math"
 	"github.com/distatus/battery"
+	"math"
+	"os"
 )
 
-type BatteryInfo struct {
-	State   battery.State
-	Percent float64
+
+
+// TODO enum structs for Markup and Align fields
+type I3BarInput struct {
+	FullText            string `json:"full_text,omitempty"`
+	ShortText           string `json:"short_text,omitempty"`
+	Color               string `json:"color,omitempty"`
+	Background          string `json:"background,omitempty"`
+	Border              string `json:"border,omitempty"`
+	BorderTop           int    `json:"border_top,omitempty"`
+	BorderRight         int    `json:"border_right,omitempty"`
+	BorderBottom        int    `json:"border_bottom,omitempty"`
+	BorderLeft          int    `json:"border_left,omitempty"`
+	MinWidth            int    `json:"min_width,omitempty"`
+	Align               string `json:"align,omitempty"`
+	Urgent              bool   `json:"urgent,omitempty"`
+	Name                string `json:"name,omitempty"`
+	Instance            string `json:"instance,omitempty"`
+	Separator           bool   `json:"separator,omitempty"`
+	SeparatorBlockWidth int    `json:"separator_block_width,omitempty"`
+	Markup              string `json:"markup,omitempty"`
 }
 
-func calc_bat_info(b *battery.Battery) (s string) {
+func BatteryInfo() (i3bi I3BarInput) {
+
+	b, err := battery.Get(0)
+	if err != nil {
+		fmt.Println("Could not get battery info!")
+		return
+	}
 
 	// bi.Percent = (b.Current / b.Full) * 100.00
 	symbol := "⚡"
 	if b.State == battery.Discharging {
 		symbol = "🔋"
 	}
-	s = fmt.Sprintf("%s %.0f%%", symbol, math.Round((b.Current / b.Full) * 100.00))
-	return s
+	i3bi.FullText = fmt.Sprintf("%s %.0f%%", symbol, math.Round((b.Current/b.Full)*100.00))
+
+	return i3bi
 }
 
 func main() {
-	battery, err := battery.Get(0)
+	b, err := json.Marshal(BatteryInfo())
 	if err != nil {
-		fmt.Println("Could not get battery info!")
-		return
+		fmt.Println("error:", err)
 	}
-	fmt.Print(calc_bat_info(battery))
+	os.Stdout.Write(b)
 
 }
